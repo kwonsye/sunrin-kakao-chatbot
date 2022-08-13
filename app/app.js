cf2c44874b52abd54ad788d784d5461d93fac5ee
kwonsye <kwonsye@naver.com>
2022-08-13 15:58:31 +0900

    배포를 위한 첫 테스트 node api

--- app/app.js
+++ app/app.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json());
+app.use(express.json());    
 
 app.post('/school-meal', (request, response)=>{
     const utterance = request.body.userRequest.utterance;
     
+    if(utterance === "오늘의 급식"){
+        const chatbotResponse = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": "오늘 날짜를 알려주세요. 예) 1월 1일"
+                        }
+                    }
+                ]
+            }
+        }
+
+        response.json(chatbotResponse)
+    }
 });
 
 app.listen(3000, ()=> console.log("node on 3000"));
